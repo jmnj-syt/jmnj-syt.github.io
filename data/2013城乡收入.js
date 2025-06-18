9bbb7bc7dc926438285caedf3065891a811a4a59
jmnj-syt <2982893682@qq.com>
2025-06-18 15:29:31 +0800

    Add files via upload

---
+++ data/2013城乡收入.js
@@ -0,0 +1,178 @@
+const compare2013={
+        "hangzhou": [
+            {
+                "name": "城镇收入",
+                "value": 43868,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 18923,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "ningbo": [
+            {
+                "name": "城镇收入",
+                "value": 45717,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 20534,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "wenzhou": [
+            {
+                "name": "城镇收入",
+                "value": 40109,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 16194,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "jiaxing": [
+            {
+                "name": "城镇收入",
+                "value": 42931,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 20556,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "huzhou": [
+            {
+                "name": "城镇收入",
+                "value": 39106,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 19044,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "shaoxing": [
+            {
+                "name": "城镇收入",
+                "value": 43941,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 19618,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "jinhua": [
+            {
+                "name": "城镇收入",
+                "value": 38848,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 14788,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "quzhou": [
+            {
+                "name": "城镇收入",
+                "value": 31620,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 11924,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "zhoushan": [
+            {
+                "name": "城镇收入",
+                "value": 40723,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 20573,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "taizhou": [
+            {
+                "name": "城镇收入",
+                "value": 39422,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 16126,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "lishui": [
+            {
+                "name": "城镇收入",
+                "value": 31627,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 10024,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ]
+    }
---
+++ data/2022城乡收入.js
@@ -0,0 +1,178 @@
+const compare2022={
+        "hangzhou": [
+            {
+                "name": "城镇收入",
+                "value": 77043,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 45183,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "ningbo": [
+            {
+                "name": "城镇收入",
+                "value": 76690,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 45487,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "wenzhou": [
+            {
+                "name": "城镇收入",
+                "value": 73326,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 38482,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "jiaxing": [
+            {
+                "name": "城镇收入",
+                "value": 72096,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 46276,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "huzhou": [
+            {
+                "name": "城镇收入",
+                "value": 71044,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 44112,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "shaoxing": [
+            {
+                "name": "城镇收入",
+                "value": 76199,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 45709,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "jinhua": [
+            {
+                "name": "城镇收入",
+                "value": 69626,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 35630,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "quzhou": [
+            {
+                "name": "城镇收入",
+                "value": 57465,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 31468,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "zhoushan": [
+            {
+                "name": "城镇收入",
+                "value": 71965,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 45924,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "taizhou": [
+            {
+                "name": "城镇收入",
+                "value": 70737,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 37700,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ],
+        "lishui": [
+            {
+                "name": "城镇收入",
+                "value": 55784,
+                "itemStyle": {
+                    "color": "#F5D242"
+                }
+            },
+            {
+                "name": "农村收入",
+                "value": 28470,
+                "itemStyle": {
+                    "color": "#F3DF99"
+                }
+            }
+        ]
+    }
